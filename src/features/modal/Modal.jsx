512ed5127739db2fc69a0de8ebeeb0c16eccaa2d
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-05-05 11:39:11 -0400

    Made the Modal title reactive

--- src/features/modal/Modal.jsx
+++ src/features/modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './CoolModal.css';
 
-function CoolModal({ children }) {
+function CoolModal({ children, magTitle }) {
   const [isOpen, setIsOpen] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
 
@@ -20,12 +20,12 @@ function CoolModal({ children }) {
 
   return (
     <div>
-      <button onClick={openModal}>Open Modal</button>
+      <button onClick={openModal} className='modalbutton'>Abrir revista de {magTitle}</button>
       {isOpen && (
         <div className={`modal-background ${isClosing ? 'fadeOut' : 'fadeIn'}`} onClick={closeModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             {children}
-            <button onClick={closeModal}>Close</button>
+            <button onClick={closeModal} className='modalbutton'>Close</button>
           </div>
         </div>
       )}
