9e8303fe6225e6236d6535a6af3f17b5a1de6a0a
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-05-05 11:42:23 -0400

    Made the header and page choice show at all pages, added offers page

--- src/root/Routes.jsx
+++ src/root/Routes.jsx
@@ -1,24 +1,36 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../features/pagestructure/Homepage.jsx";
-import Showcase from "../features/magazineshowcase/components/MagazineShowcase.jsx";
 import CopyrightToggler from "../features/copyright/components/CopyrightToggler.jsx";
 import CoolModal from "../features/modal/Modal.jsx";
-import FarmStore  from '../features/farmstore/components/FarmStore.jsx';
+import FarmStore from "../features/farmstore/components/FarmStore.jsx";
 import Chatbot from "../features/chatbot/components/Chatbot.jsx";
 import OffersPage from "../features/offerscont/OffersPage.jsx";
-
+import Header from '../features/header/components/Header'
+import PageChoice from '../features/mainpage/components/PageChoice';
+import Footer from '../features/footer/components/Footer'
+import OfferDisplay1 from "../features/offersmod/OfferDisplay1.jsx";
+import OfferDisplay2 from "../features/offersmod/OfferDisplay2.jsx";
+import OfferDisplay3 from "../features/offersmod/OfferDisplay3.jsx";
 
 function Routing() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/mag" element={<Showcase />} />
-      <Route path="/Copyright" element={<CopyrightToggler />} />
-      <Route path="/modal" element={<CoolModal />} />
-      <Route path="/farm" element={<FarmStore />} />
-      <Route path="/cb" element={<Chatbot />} />
-      <Route path="/offer" element={<OffersPage />} />
-    </Routes>
+    <div>
+      <Header />
+      <PageChoice />
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/offers" element={<OffersPage />} />
+        <Route path="/Copyright" element={<CopyrightToggler />} />
+        <Route path="/modal" element={<CoolModal />} />
+        <Route path="/farm" element={<FarmStore />} />
+        <Route path="/cb" element={<Chatbot />} />
+        <Route path="/offers/BECENSE" element={<OfferDisplay1 />} />
+        <Route path="/offers/Bravo" element={<OfferDisplay2 />} />
+        <Route path="/offers/Sirena" element={<OfferDisplay3 />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
