e72d7a6014977affcab9cd4f2c221d318d7cd827
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-05-05 11:36:23 -0400

    Changed Header name (how did i didnt notice? lmao)

--- src/features/header/components/Header.jsx
+++ src/features/header/components/Header.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faYoutube, faInstagram, faFacebook, } from "@fortawesome/free-brands-svg-icons";
 import logo from "/logo.png";
 import SearchToggle from './SearchToggle'
 
 
 
-const Footer = () => {
+const Header = () => {
  
 
   var logo1 = (
     <img
-      //src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg"
       src={logo}
       alt="Canvas Logo"
       className="logo"
@@ -23,52 +22,16 @@ const Footer = () => {
 
   return (
     <header className="main-header">
+      <Link to='/'>
       <label htmlFor="btn-nav" className="btn-nav">
-        <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon icon={faHome} /> 
       </label>
+      </Link>
       <input type="checkbox" id="btn-nav" />
       {logo1}
       <h2 className="pageName">PromoSpot</h2>
 
-      <nav className="navigation">
-        <ul className="menu">
-          <li className="Loterias">
-            <a href="#">
-              <Link to="/farm">Loterias</Link>
-            </a>
-            <ul className="submenu uno">
-              <li className="li Leidsa">Leidsa</li>
-
-              <li>Lotoreal</li>
-
-              <li>Loteka</li>
-            </ul>
-          </li>
-          <li className="Farmacias dos">
-            <a href="#">Farmacias</a>
-            <ul className="submenu">
-              <li>Carol</li>
-              <li>Hidalgo</li>
-            </ul>
-          </li>
-          <li className="Supermercados tres">
-            <a href="#">Supermercados</a>
-            <ul className="submenu">
-              <li>Bravo</li>
-
-              <li>Olé</li>
-
-              <li>Sirena</li>
-
-              <li>Nacional</li>
-
-              <li>Jumbo</li>
-
-              <li>SuperPola</li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+      
 
       <div className="searchcont">
         <div className="iconcont">
@@ -93,4 +56,4 @@ const Footer = () => {
     </header>
   );
 };
-export default Footer;
+export default Header;
