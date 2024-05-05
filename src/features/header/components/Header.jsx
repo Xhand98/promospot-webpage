c62708f0ea45b733ebb6cf92ffd5ca51723b3d9d
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-05-04 20:48:31 -0400

    Formatted header.jsx

--- src/features/header/components/Header.jsx
+++ src/features/header/components/Header.jsx
@@ -1,93 +1,121 @@
-import React from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faYoutube,  faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
-import logo from '/logo.png'
-    
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faYoutube,
+  faInstagram,
+  faFacebookF,
+} from "@fortawesome/free-brands-svg-icons";
+import logo from "/logo.png";
+
+const Searchbar = () => {
+  return (
+    <input
+      type="text"
+      placeholder="¿Que buscas?"
+      className="buscador"
+      id="searchbar"
+      onKeyDown={handleKeyPress}
+    />
+  );
+};
 
 const Footer = () => {
+  const handleKeyPress = (event) => {
+    var barra = document.getElementById("searchbar");
 
-    const handleKeyPress = (event) => {
-        var barra = document.getElementById('searchbar')
+    var barravalue = barra.value;
 
-        var barravalue = barra.value
+    if (event.key === "Enter") {
+      event.preventDefault();
 
-        if (event.key === 'Enter') {
-            event.preventDefault();
+      console.log(barravalue);
+      alert(`No hemos encontrado ${barravalue}`);
+    }
+  };
 
-            console.log(barravalue);
-            alert(`No hemos encontrado ${barravalue}`)
-        }
-    };
+  var logo1 = (
+    <img
+      //src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg"
+      src={logo}
+      alt="Canvas Logo"
+      className="logo"
+    />
+  );
 
-    var logo1 = (
-        <img 
-          //src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg"
-          src={logo}
-          alt="Canvas Logo"
-          className='logo'
-        />
-    )
-   
+  return (
+    <header className="main-header">
+      <label htmlFor="btn-nav" className="btn-nav">
+        <FontAwesomeIcon icon={faBars} />
+      </label>
+      <input type="checkbox" id="btn-nav" />
+      {logo1}
+      <h2 className="pageName">PromoSpot</h2>
 
-    return (
-        <header className="main-header">
-    <label htmlFor="btn-nav" className="btn-nav"><FontAwesomeIcon icon={ faBars }/></label>
-    <input type="checkbox" id="btn-nav" />
-    { logo1 }
-    <h2 className="pageName">PromoSpot</h2>
-
-    <nav className="navigation">
+      <nav className="navigation">
         <ul className="menu">
-            <li className="Loterias"><a href="#"><Link to="/farm">Loterias</Link></a>
-                <ul className="submenu uno" >
-                    <li className="li Leidsa">Leidsa</li>
-                    
-                    <li>Lotoreal</li>
-                  
-                    <li>Loteka</li>
-                </ul>
-            </li>
-            <li className="Farmacias dos"><a href="#">Farmacias</a>
-                <ul className="submenu">
-                    <li>Carol</li>
-                    <li>Hidalgo</li>
-                </ul>
-            </li>
-            <li className="Supermercados tres"><a href="#">Supermercados</a>
-                <ul className="submenu">
-                    <li>Bravo</li>
+          <li className="Loterias">
+            <a href="#">
+              <Link to="/farm">Loterias</Link>
+            </a>
+            <ul className="submenu uno">
+              <li className="li Leidsa">Leidsa</li>
 
-                    <li>Olé</li>
+              <li>Lotoreal</li>
 
-                    <li>Sirena</li>
+              <li>Loteka</li>
+            </ul>
+          </li>
+          <li className="Farmacias dos">
+            <a href="#">Farmacias</a>
+            <ul className="submenu">
+              <li>Carol</li>
+              <li>Hidalgo</li>
+            </ul>
+          </li>
+          <li className="Supermercados tres">
+            <a href="#">Supermercados</a>
+            <ul className="submenu">
+              <li>Bravo</li>
 
-                    <li>Nacional</li>
+              <li>Olé</li>
 
-                    <li>Jumbo</li>
-                    
-                    <li>SuperPola</li>
-                </ul>
-            </li>
+              <li>Sirena</li>
+
+              <li>Nacional</li>
+
+              <li>Jumbo</li>
+
+              <li>SuperPola</li>
+            </ul>
+          </li>
         </ul>
+      </nav>
 
-    </nav>
-
-   
-
-   <div className="searchcont"> 
-  <div className='iconcont'>
-  <FontAwesomeIcon icon={faMagnifyingGlass} />
-   <input type="text" placeholder="¿Que buscas?" className="buscador" id='searchbar' onKeyDown={handleKeyPress} />
-   <a href=''><i><FontAwesomeIcon icon={ faYoutube }/></i></a>
-   <a href=''><i><FontAwesomeIcon icon={ faInstagram }/></i></a>
-   <a href=''><i><FontAwesomeIcon icon={ faFacebookF }/></i></a>
-   </div>
-   </div>
-</header>
-
-    );
+      <div className="searchcont">
+        <div className="iconcont">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <Searchbar />
+          <a href="">
+            <i>
+              <FontAwesomeIcon icon={faYoutube} />
+            </i>
+          </a>
+          <a href="">
+            <i>
+              <FontAwesomeIcon icon={faInstagram} />
+            </i>
+          </a>
+          <a href="">
+            <i>
+              <FontAwesomeIcon icon={faFacebookF} />
+            </i>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 export default Footer;
