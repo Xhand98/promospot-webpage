035efc1a26c1eacd8c34c52ec4798513909fdaa8
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-05-05 11:41:33 -0400

    Elim unused components and added the showcase by default

--- src/features/pagestructure/Homepage.jsx
+++ src/features/pagestructure/Homepage.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./homepage.css";
 import { Sidetab } from '@typeform/embed-react'
-import Footer from "../footer/components/Footer.jsx";
 import ChatbotToggler from "../chatbot/components/ChatbotToggler.jsx";
-import Header from "../header/components/Header.jsx";
 import MainPage from "../mainpage/components/MainPage.jsx";
+import Showcase from '../magazineshowcase/components/MagazineShowcase'
 
 function HomePage() {
   return (
     <div className="mainPage">
-      <Header />
       <MainPage />
       <Sidetab id="neNTaGfQ" buttonText="click to open" />
-      <Footer />
+      <Showcase />
       <ChatbotToggler />
     </div>
   );
